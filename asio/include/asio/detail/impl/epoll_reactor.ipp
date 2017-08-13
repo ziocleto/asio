ziocleto/asio

commit 669e6b8b9de1309927b29d8b6be3630cc69c07ac

Don't free the reactor's per-descriptor state until after close.

asio/include/asio/detail/impl/epoll_reactor.ipp
@@ -358,9 +358,6 @@ void epoll_reactor::deregister_descriptor(socket_type descriptor,
 
     descriptor_lock.unlock();
 
-    free_descriptor_state(descriptor_data);
-    descriptor_data = 0;
-
     io_service_.post_deferred_completions(ops);
   }
 }
@@ -386,7 +383,14 @@ void epoll_reactor::deregister_internal_descriptor(socket_type descriptor,
     descriptor_data->shutdown_ = true;
 
     descriptor_lock.unlock();
+  }
+}
 
+void epoll_reactor::cleanup_descriptor_data(
+    per_descriptor_data& descriptor_data)
+{
+  if (descriptor_data)
+  {
     free_descriptor_state(descriptor_data);
     descriptor_data = 0;
   }

asio/include/asio/detail/impl/kqueue_reactor.ipp
@@ -311,9 +311,6 @@ void kqueue_reactor::deregister_descriptor(socket_type descriptor,
 
     descriptor_lock.unlock();
 
-    free_descriptor_state(descriptor_data);
-    descriptor_data = 0;
-
     io_service_.post_deferred_completions(ops);
   }
 }
@@ -343,7 +340,14 @@ void kqueue_reactor::deregister_internal_descriptor(socket_type descriptor,
     descriptor_data->shutdown_ = true;
 
     descriptor_lock.unlock();
+  }
+}
 
+void kqueue_reactor::cleanup_descriptor_data(
+    per_descriptor_data& descriptor_data)
+{
+  if (descriptor_data)
+  {
     free_descriptor_state(descriptor_data);
     descriptor_data = 0;
   }

asio/include/asio/detail/dev_poll_reactor.hpp
@@ -101,14 +101,20 @@ public:
   ASIO_DECL void cancel_ops(socket_type descriptor, per_descriptor_data&);
 
   // Cancel any operations that are running against the descriptor and remove
-  // its registration from the reactor.
+  // its registration from the reactor. The reactor resources associated with
+  // the descriptor must be released by calling cleanup_descriptor_data.
   ASIO_DECL void deregister_descriptor(socket_type descriptor,
       per_descriptor_data&, bool closing);
 
-  // Cancel any operations that are running against the descriptor and remove
-  // its registration from the reactor.
+  // Remove the descriptor's registration from the reactor. The reactor
+  // resources associated with the descriptor must be released by calling
+  // cleanup_descriptor_data.
   ASIO_DECL void deregister_internal_descriptor(
       socket_type descriptor, per_descriptor_data&);
+
+  // Perform any post-deregistration cleanup tasks associated with the
+  // descriptor data.
+  ASIO_DECL void cleanup_descriptor_data(per_descriptor_data&);
 
   // Add a new timer queue to the reactor.
   template <typename Time_Traits>

asio/include/asio/detail/epoll_reactor.hpp
@@ -122,13 +122,21 @@ public:
       per_descriptor_data& descriptor_data);
 
   // Cancel any operations that are running against the descriptor and remove
-  // its registration from the reactor.
+  // its registration from the reactor. The reactor resources associated with
+  // the descriptor must be released by calling cleanup_descriptor_data.
   ASIO_DECL void deregister_descriptor(socket_type descriptor,
       per_descriptor_data& descriptor_data, bool closing);
 
-  // Remote the descriptor's registration from the reactor.
+  // Remove the descriptor's registration from the reactor. The reactor
+  // resources associated with the descriptor must be released by calling
+  // cleanup_descriptor_data.
   ASIO_DECL void deregister_internal_descriptor(
       socket_type descriptor, per_descriptor_data& descriptor_data);
+
+  // Perform any post-deregistration cleanup tasks associated with the
+  // descriptor data.
+  ASIO_DECL void cleanup_descriptor_data(
+      per_descriptor_data& descriptor_data);
 
   // Add a new timer queue to the reactor.
   template <typename Time_Traits>

asio/include/asio/detail/impl/dev_poll_reactor.ipp
@@ -234,6 +234,11 @@ void dev_poll_reactor::deregister_internal_descriptor(
     op_queue_[i].cancel_operations(descriptor, ops, ec);
 }
 
+void dev_poll_reactor::cleanup_descriptor_data(
+    dev_poll_reactor::per_descriptor_data&)
+{
+}
+
 void dev_poll_reactor::run(bool block, op_queue<operation>& ops)
 {
   asio::detail::mutex::scoped_lock lock(mutex_);

asio/include/asio/detail/impl/reactive_descriptor_service.ipp
@@ -87,10 +87,12 @@ void reactive_descriptor_service::destroy(
 
     reactor_.deregister_descriptor(impl.descriptor_, impl.reactor_data_,
         (impl.state_ & descriptor_ops::possible_dup) == 0);
-  }
 
-  asio::error_code ignored_ec;
-  descriptor_ops::close(impl.descriptor_, impl.state_, ignored_ec);
+    asio::error_code ignored_ec;
+    descriptor_ops::close(impl.descriptor_, impl.state_, ignored_ec);
+
+    reactor_.cleanup_descriptor_data(impl.reactor_data_);
+  }
 }
 
 asio::error_code reactive_descriptor_service::assign(
@@ -127,9 +129,15 @@ asio::error_code reactive_descriptor_service::close(
 
     reactor_.deregister_descriptor(impl.descriptor_, impl.reactor_data_,
         (impl.state_ & descriptor_ops::possible_dup) == 0);
-  }
 
-  descriptor_ops::close(impl.descriptor_, impl.state_, ec);
+    descriptor_ops::close(impl.descriptor_, impl.state_, ec);
+
+    reactor_.cleanup_descriptor_data(impl.reactor_data_);
+  }
+  else
+  {
+    ec = asio::error_code();
+  }
 
   // The descriptor is closed by the OS even if close() returns an error.
   //
@@ -153,6 +161,7 @@ reactive_descriptor_service::release(
     ASIO_HANDLER_OPERATION(("descriptor", &impl, "release"));
 
     reactor_.deregister_descriptor(impl.descriptor_, impl.reactor_data_, false);
+    reactor_.cleanup_descriptor_data(impl.reactor_data_);
     construct(impl);
   }
 

asio/include/asio/detail/impl/reactive_socket_service_base.ipp
@@ -88,6 +88,8 @@ void reactive_socket_service_base::destroy(
 
     asio::error_code ignored_ec;
     socket_ops::close(impl.socket_, impl.state_, true, ignored_ec);
+
+    reactor_.cleanup_descriptor_data(impl.reactor_data_);
   }
 }
 
@@ -101,9 +103,15 @@ asio::error_code reactive_socket_service_base::close(
 
     reactor_.deregister_descriptor(impl.socket_, impl.reactor_data_,
         (impl.state_ & socket_ops::possible_dup) == 0);
-  }
 
-  socket_ops::close(impl.socket_, impl.state_, false, ec);
+    socket_ops::close(impl.socket_, impl.state_, false, ec);
+
+    reactor_.cleanup_descriptor_data(impl.reactor_data_);
+  }
+  else
+  {
+    ec = asio::error_code();
+  }
 
   // The descriptor is closed by the OS even if close() returns an error.
   //

asio/include/asio/detail/impl/select_reactor.ipp
@@ -162,6 +162,11 @@ void select_reactor::deregister_internal_descriptor(
     op_queue_[i].cancel_operations(descriptor, ops);
 }
 
+void select_reactor::cleanup_descriptor_data(
+    select_reactor::per_descriptor_data&)
+{
+}
+
 void select_reactor::run(bool block, op_queue<operation>& ops)
 {
   asio::detail::mutex::scoped_lock lock(mutex_);

commit 95abd6a4d82780ed5a6e4bcb5aabd20a611b101e

Fix double delete issue introduced by 669e6b8b9de1309927b29d8b6be3630cc69c07ac.

asio/include/asio/detail/impl/epoll_reactor.ipp
@@ -359,6 +359,15 @@ void epoll_reactor::deregister_descriptor(socket_type descriptor,
     descriptor_lock.unlock();
 
     io_service_.post_deferred_completions(ops);
+
+    // Leave descriptor_data set so that it will be freed by the subsequent
+    // call to cleanup_descriptor_data.
+  }
+  else
+  {
+    // We are shutting down, so prevent cleanup_descriptor_data from freeing
+    // the descriptor_data object and let the destructor free it instead.
+    descriptor_data = 0;
   }
 }
 
@@ -383,6 +392,15 @@ void epoll_reactor::deregister_internal_descriptor(socket_type descriptor,
     descriptor_data->shutdown_ = true;
 
     descriptor_lock.unlock();
+
+    // Leave descriptor_data set so that it will be freed by the subsequent
+    // call to cleanup_descriptor_data.
+  }
+  else
+  {
+    // We are shutting down, so prevent cleanup_descriptor_data from freeing
+    // the descriptor_data object and let the destructor free it instead.
+    descriptor_data = 0;
   }
 }
 

asio/include/asio/detail/impl/kqueue_reactor.ipp
@@ -312,6 +312,15 @@ void kqueue_reactor::deregister_descriptor(socket_type descriptor,
     descriptor_lock.unlock();
 
     io_service_.post_deferred_completions(ops);
+
+    // Leave descriptor_data set so that it will be freed by the subsequent
+    // call to cleanup_descriptor_data.
+  }
+  else
+  {
+    // We are shutting down, so prevent cleanup_descriptor_data from freeing
+    // the descriptor_data object and let the destructor free it instead.
+    descriptor_data = 0;
   }
 }
 
@@ -340,6 +349,15 @@ void kqueue_reactor::deregister_internal_descriptor(socket_type descriptor,
     descriptor_data->shutdown_ = true;
 
     descriptor_lock.unlock();
+
+    // Leave descriptor_data set so that it will be freed by the subsequent
+    // call to cleanup_descriptor_data.
+  }
+  else
+  {
+    // We are shutting down, so prevent cleanup_descriptor_data from freeing
+    // the descriptor_data object and let the destructor free it instead.
+    descriptor_data = 0;
   }
 }
 